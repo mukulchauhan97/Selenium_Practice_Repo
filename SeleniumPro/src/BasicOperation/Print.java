2821881512a7f3604a7a5f23b122976e6018c698
Mukul <mukul.b.chauhan@accenture.com>
2023-09-28 12:27:37 +0530

    My First program

---
+++ SeleniumPro/src/BasicOperation/Print.java
@@ -0,0 +1,10 @@
+package BasicOperation;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Something");
+	}
+
+}
---
+++ SeleniumPro/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mukul.b.chauhan
+ *
+ */
+module SeleniumPro {
+}
